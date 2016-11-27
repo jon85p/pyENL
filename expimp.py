9d20b1e03576f3b64ca9ffff5c11bcd8c465768b
jon85p <jhonvalencia314@gmail.com>
2016-11-27 14:38:50 -0500

    :sparkles: Interacción con otras herramientas, primer commit

---
+++ expimp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Funciones para exportar e importar datos'''
+
+def sols2odt(sols):
+    '''
+    Soluciones a documento Open Document Text
+    '''
+    pass
+
+def sols2tex(sols):
+    '''
+    Soluciones a documento TeX
+    '''
+    pass
+
+def import_ees(file):
+    '''
+    Importar texto de documentos EES y convertirlos a formato pyENL
+    '''
+    pass
